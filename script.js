d395f3a4fc37fea7bd01ab41caae1555b1370e98
flooprea <62398104+flooprea@users.noreply.github.com>
2020-04-08 22:12:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => {
+    var temp = Math.floor(Math.random()*10);
+    console.log(temp);
+     return temp;
+}
+
+const compareGuesses = (userGuess, computerGuess, target) =>{
+    console.log('user:' + userGuess);
+    console.log('comp:' +computerGuess);
+    console.log('target:' +target);
+ 
+    if(userGuess === computerGuess){
+        console.log('equals');
+        return true;
+    }
+   if(Math.abs(target - computerGuess) > Math.abs(target - userGuess)){
+        console.log('comp');
+        return true;
+    } else{
+        return false;
+    }
+}
+    
+const updateScore = winner =>{
+    if(winner === 'human'){
+        humanScore++;
+    }else if(winner === 'computer'){
+        computerScore++;
+    }
+}
+
+const advanceRound = () =>{
+    currentRoundNumber++;
+} 
